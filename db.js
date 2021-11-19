f990cc257b0033b7d9af347d078307222ab992b5
Veljko <veljkoilik@gmail.com>
2021-11-19 01:22:12 +0100

    32ss2

--- db.js
+++ db.js
@@ -6,10 +6,10 @@ dotenv.config({
 });
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
 });
 db.connect(function (err) {
   if (err) {
