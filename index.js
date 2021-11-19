a4e3dee5917ced874c3232dc9a2e6b62fc1bfccb
Veljko <veljkoilik@gmail.com>
2021-11-19 15:02:43 +0100

    1\1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://moja-kuhinja.netlify.app/",
+  })
+);
 
 const PORT = 3001;
 //registration
