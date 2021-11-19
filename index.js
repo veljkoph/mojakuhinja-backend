e54e9df39a505979bb1d9e0ffddc4660ffe5366e
Veljko <veljkoilik@gmail.com>
2021-11-19 15:10:47 +0100

    asaSas1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://moja-kuhinja.netlify.app/",
+    origin: "https://moja-kuhinja.netlify.app/*",
   })
 );
 
@@ -11,7 +11,7 @@ const PORT = 3001;
 //registration
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", cors.origin);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
