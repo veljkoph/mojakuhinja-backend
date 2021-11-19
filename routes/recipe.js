e717b46a2ccc99cd2133e7e76571281cee916169
Veljko <veljkoilik@gmail.com>
2021-11-19 15:05:12 +0100

    lll1

--- routes/recipe.js
+++ routes/recipe.js
@@ -240,15 +240,15 @@ router.get("/savedrecipes", (req, res) => {
 });
 
 // //getting all recipes from database
-// router.get("/all", (req, res) => {
-//   const sqlGet = "SELECT * FROM recipes LIMIT 50";
-//   db.query(sqlGet, (err, result) => {
-//     res.send(result);
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-// });
+router.get("/all", (req, res) => {
+  const sqlGet = "SELECT * FROM recipes LIMIT 50";
+  db.query(sqlGet, (err, result) => {
+    res.send(result);
+    if (err) {
+      console.log(err);
+    }
+  });
+});
 
 //search by title
 router.get("/byname/:name", (req, res) => {
