0748ca7550a29e4b9a88ec30e4162c798af08a64
Veljko <veljkoilik@gmail.com>
2021-11-19 16:34:13 +0100

    asas1

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const PORT = 3001;
 //registration
 
 app.use(function (req, res, next) {
-  req.header("Access-Control-Allow-Origin", "*");
-  req.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  req.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
   next();
 });
 
